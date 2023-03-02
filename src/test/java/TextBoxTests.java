d051a39e7e8b51af37889f1e2c341a2483f96d3c
zhuravlev.mikhail <miha99_66@mail.ru>
2023-03-02 13:31:31 +0300

    last version

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -4,38 +4,42 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1280";
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.screenshots = false;
     }
 
     @Test
     void fillTestForm() {
         open("/automation-practice-form");
-        $("[id='firstName']").setValue("Mikhail");
-        $("[id='lastName']").setValue("Zhuravlev");
-        $("[id='userEmail']").setValue("miha99_66@mail.ru");
-        $("[for='gender-radio-1']").click();
-        $("[id='userNumber']").setValue("9126298333");
+        $("#firstName").setValue("Mikhail");
+        $("#lastName").setValue("Zhuravlev");
+        $("#userEmail").setValue("miha99_66@mail.ru");
+        $("[for=gender-radio-1]").click();
+        $("#userNumber").setValue("9126298333");
 
-        $("[id='dateOfBirthInput']").click();
-        $("[class='react-datepicker__month-select']").click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
         $("[value='4']").click();
-        $("[class='react-datepicker__year-select']").click();
+        $(".react-datepicker__year-select").click();
         $("[value='1988']").click();
         $("[aria-label='Choose Friday, May 27th, 1988']").click();
 
-        $("[id='subjectsInput']").setValue("e").pressEnter();
+        $("#subjectsInput").setValue("engl").pressEnter();
         $("[for='hobbies-checkbox-3']").click();
+        $("[for='hobbies-checkbox-2']").click();
 
         $("input[type='file']").uploadFile(new File("src/test/resources/CV.jpg"));
 
-        $("[id='currentAddress']").setValue("Kras");
+        $("#currentAddress").setValue("Kras");
 
         $("#state").click();
         $("#react-select-3-option-0").click();
@@ -43,8 +47,17 @@ public class TextBoxTests {
         $("#city").click();
         $("#react-select-4-option-0").click();
 
-        sleep(5000);
+        $("#submit").click();
 
-       // int x = 0;
+        $(".modal-body").shouldHave(text("Mikhail Zhuravlev"),
+                text("miha99_66@mail.ru"),
+                text("Male"),
+                text("9126298333"),
+                text("27 May,1988"),
+                text("English"),
+                text("Music"),
+                text("CV.jpg"),
+                text("Kras"),
+                text("NCR Delhi"));
     }
 }
